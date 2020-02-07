cd3bc11134063d5cba5565caf0798f6d68a97e4d
Inna Widener <iwide2019@gmail.com>
2020-02-06 20:28:20 -0600

    Time in navbar working, deleted two files, commented script.js as the codes in process

--- script.js
+++ script.js
@@ -4,70 +4,65 @@
 $(document).ready(function () {
 
     console.log("ready");
-    
-//Code for weather with click button
-//Need to download the images for this part of code from openweather website and
-//work on click button code if there is no button in index file. Need to change the names and ids.
 
-    $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather",
-        dataType: "json",
-        method: "GET",
-        data: { q: city, appid: apiKey, units: "imperial" },
+    //Code for date in moment.js
 
-        success: function (data) {
-            console.log(data);
-            var forecast = "";
-            $.each(data.weather, function (index, val) {
-                forecast += "<p><b>" + data.name + "</b><img src=" + val.icon + ".png></p>" +
-                    data.main.temperature + "&deg;F" + "|" + val.main + ", " + val.description
-            });
+    var m = moment();
+    var curDay = m.format("dddd, MMMM Do YYYY h:mm:ss a");
+    console.log(m.format("dddd, MMMM Do YYYY h:mm:ss a"));
 
-            $("#current-section").html(forecast);
-        }
-    })
+    $("#currentDay").text(curDay);
 })
 
-//Showing current time using moment.js
-//Creating id in index html #currentDay inside header and
-//Creating in Gitbash another file as moment.js
+    //Code for weather with click button
+    //Need to download the images for this part of code from openweather website and
+    //work on click button code if there is no button in index file. Need to change the names and ids.
 
-$("#currentDay").text(curDay);
+//    //$.ajax({
+//         url: "https://api.openweathermap.org/data/2.5/weather",
+//         dataType: "json",
+//         method: "GET",
+//         data: { q: city, appid: apiKey, units: "imperial" },
 
-//Code for date in moment.js
-//link in index file at the bottom pf a page:
-// <script src="moment.js"></script>
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+//         success: function (data) {
+//             console.log(data);
+//             var forecast = "";
+//             $.each(data.weather, function (index, val) {
+//                 forecast += "<p><b>" + data.name + "</b><img src=" + val.icon + ".png></p>" +
+//                     data.main.temperature + "&deg;F" + "|" + val.main + ", " + val.description
+//             });
 
-var m = moment();
-var curDay = m.format("dddd, MMMM Do YYYY h:mm:ss a");
-console.log(m.format("dddd, MMMM Do YYYY h:mm:ss a"));
+//             $("#current-section").html(forecast);
+//         }
+//     })
+// })
 
-//Div class container should have two div ids:
-// div id = "start-screen" with a background and button <button id="start" class="start-btn">Go!</button>
-$(".start-btn").on("click", function (event) {
-    event.preventDefault();
-    //div id = "main-screen"
-   // $( ".main-screen" ).hide();
-})
 
-//Local Storage //Put in the very beginning
-var events = ["","","","","","","","",""]
-events = JSON.parse(localStorage.getItem("events")) || {};
+// //Div class container should have two div ids:
+// // div id = "start-screen" with a background and button <button id="start" class="start-btn">Go!</button>
+// $(".start-btn").on("click", function (event) {
+//     event.preventDefault();
+//     //div id = "main-screen"
+//     // $( ".main-screen" ).hide();
+// })
 
-//Local Storage//Can be placed in a different place
+// //Local Storage //Put in the very beginning
+// var events = ["", "", "", "", "", "", "", "", ""]
+// events = JSON.parse(localStorage.getItem("events")) || {};
 
-$(document).on("click", ".saveBtn", function (event) {
-    event.preventDefault();
-    console.log(this);
-    var userInput = $(this).siblings(".description").val();
+// //Local Storage//Can be placed in a different place
 
-    var hour = $(this).attr("id");
+// $(document).on("click", ".saveBtn", function (event) {
+//     event.preventDefault();
+//     console.log(this);
+//     var userInput = $(this).siblings(".description").val();
 
-    console.log("hour", hour);
-    events[hour] = userInput;
-    console.log("events", events);
-    localStorage.setItem("events", JSON.stringify(events));
+//     var hour = $(this).attr("id");
 
-})
-});
+//     console.log("hour", hour);
+//     events[hour] = userInput;
+//     console.log("events", events);
+//     localStorage.setItem("events", JSON.stringify(events));
+
+// })
+
