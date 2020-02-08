5d5680ec5a1a6ef87be1051823ed29882f024f74
Colby Norman <colbynorm41@gmail.com>
2020-02-08 11:44:08 -0600

    added Local Storage and Student Name populates in Div

--- modal.js
+++ modal.js
@@ -1,5 +1,20 @@
+// Modal //
 $(document).ready(function(){
   $("#myBtn").click(function(){
     $("#myModal").modal();
   });
+  // Local Storage //
+  var saveButton = $(".loginBtn");
+  console.log(saveButton);
+  saveButton.on("click", function(event) {
+    event.preventDefault();
+    var studentName = $('#name').val(); 
+    localStorage.setItem('student-name', studentName);
+    console.log(studentName);
+    var studentItem = $("<li>");
+    studentItem.text(studentName);
+    $("#student").append(studentItem);
+  });
+  // Append Name's to Student Name Div //
+  
 });
